6bb2ef461dd445daaa0dff5463bcfcd2e052abc8
Nwachukwu Francis <francisnwachukwu100@gmail.com>
2023-01-23 17:34:00 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <div className="w-screen h-screen flex flex-items justify-center">
+        App
+      </div>
+    </div>
+  );
+};
+
+export default App;
